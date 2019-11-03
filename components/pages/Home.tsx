3f79b892d18118cccf1a20c91c26a44f9f2a4a49
Theodor Vararu <theo@vararu.org>
2019-11-03 14:24:56 +0000

    Hook up Player to album view

--- components/pages/Home.tsx
+++ components/pages/Home.tsx
@@ -1,5 +1,13 @@
 import Player from "../lib/Player";
 
-const Home = () => <Player />;
+const files = [
+  "/static/music/Clams Casino/Rainforest/01 Natural.mp3",
+  "/static/music/Clams Casino/Rainforest/02 Treetop.mp3",
+  "/static/music/Clams Casino/Rainforest/03 Waterfalls.mp3",
+  "/static/music/Clams Casino/Rainforest/04 Drowning.mp3",
+  "/static/music/Clams Casino/Rainforest/05 Gorilla.mp3"
+];
+
+const Home = () => <Player files={files} />;
 
 export default Home;
